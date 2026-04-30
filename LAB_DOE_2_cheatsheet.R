a2599c17f7c15f73fe06bec23284dda91641a85f
Camilo Acosta <43940770+acosmilo@users.noreply.github.com>
2026-04-30 06:43:39 -0500

    Create LAB_DOE_2_cheatsheet.R

---
+++ LAB_DOE_2_cheatsheet.R
@@ -0,0 +1,59 @@
+# PRUEBAS T DE DOS MUESTRAS EN R
+
+# 1. DATOS DE EJEMPLO
+
+
+# Muestras de igual tamaño
+x1 <- c(12, 14, 15, 13, 16)
+y1 <- c(10, 11, 14, 12, 13)
+
+# Muestras de diferente tamaño
+x2 <- c(12, 14, 15, 13, 16, 18, 17)
+y2 <- c(10, 11, 14, 12)
+# 2. PRUEBAS NO PAREADAS (INDEPENDIENTES)
+
+
+# 2.1 Varianzas diferentes (Welch, opción por defecto en R)
+
+# Bilateral (H0: medias iguales)
+t.test(x1, y1, var.equal = FALSE, alternative = "two.sided")
+
+# Unilateral derecha (H1: media de x > media de y)
+t.test(x1, y1, var.equal = FALSE, alternative = "greater")
+
+# Unilateral izquierda (H1: media de x < media de y)
+t.test(x1, y1, var.equal = FALSE, alternative = "less")
+
+# Con muestras de diferente tamaño
+t.test(x2, y2, var.equal = FALSE)
+
+# 2.2 Varianzas iguales (t de Student clásico)
+
+# Bilateral
+t.test(x1, y1, var.equal = TRUE, alternative = "two.sided")
+
+# Unilateral derecha
+t.test(x1, y1, var.equal = TRUE, alternative = "greater")
+
+# Unilateral izquierda
+t.test(x1, y1, var.equal = TRUE, alternative = "less")
+
+# Con muestras de diferente tamaño
+t.test(x2, y2, var.equal = TRUE)
+
+
+#PRUEBAS PAREADAS (DEPENDIENTES)
+
+# Bilateral
+t.test(x1, y1, paired = TRUE, alternative = "two.sided")
+
+# Unilateral derecha (x > y)
+t.test(x1, y1, paired = TRUE, alternative = "greater")
+
+# Unilateral izquierda (x < y)
+t.test(x1, y1, paired = TRUE, alternative = "less")
+
+
+#     no permitidas en pruebas pareadas
+
+############################################################
